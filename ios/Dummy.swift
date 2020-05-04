d0b76e2bae618d261f65dfd1a6c55e7bb33d2097
Bramus <bramus@bram.us>
2020-05-04 17:18:40 +0200

    ⬆️ react-native 0.62.2
    $ react-native upgrade
    + lots of manual changes
    
    @ref:
    - https://react-native-community.github.io/upgrade-helper/?from=0.61.5&to=0.62.2
    - https://github.com/react-native-community/upgrade-support/issues/11#issuecomment-604965909
    - https://github.com/react-native-community/upgrade-support/issues/13
    - https://github.com/react-native-community/upgrade-support/issues/25

---
+++ ios/Dummy.swift
@@ -0,0 +1,11 @@
+//
+//  Dummy.swift
+//  RNMapsExample
+//
+//  Created by Bramus on 04/05/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+
+// @ref https://github.com/react-native-community/upgrade-support/issues/25
